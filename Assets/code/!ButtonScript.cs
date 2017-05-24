e72b9315e66ad44740ca17a3f8dd1972db8c19dc
Azunyan1111 <admin@admin.local>
2017-05-24 16:48:53 +0900

    frist window

---
+++ Assets/code/!ButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	// resize button
+	public void Awake(){
+		Debug.Log("frist start");
+	}
+
+	/// ボタンをクリックした時の処理
+	public void OnClick() {
+		Debug.Log("Button click!");
+	}
+}
