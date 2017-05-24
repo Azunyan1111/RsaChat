d27a0d68e2b7d25e95e0845471e0d992a9280e23
Azunyan <s15a1302@bene.fit.ac.jp>
2017-05-24 21:55:56 +0900

    Add NewAddress Scene.

---
+++ Assets/code/GoToNewAddressSceneButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoToNewAddressSceneButton : MonoBehaviour {
+
+	public void OnClick() {
+		SceneManager.LoadScene("NewAddress");
+	}
+}
