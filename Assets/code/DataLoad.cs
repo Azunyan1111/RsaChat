e14b13c7267858a5210daa3b5e1afef39060460a
Azunyan <s15a1302@bene.fit.ac.jp>
2017-05-25 16:31:46 +0900

    add load window

---
+++ Assets/code/DataLoad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("NextScene", 2.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void NextScene(){
+		// TODO: load main window
+		SceneManager.LoadScene("Start");
+	}
+}
