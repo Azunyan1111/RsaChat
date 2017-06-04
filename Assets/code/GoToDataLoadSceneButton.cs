c2b69913053a23939a3d405560211b94a8c37cd9
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2017-06-05 03:23:21 +0900

    add signup http post.

--- Assets/code/GoToDataLoadSceneButton.cs
+++ Assets/code/GoToDataLoadSceneButton.cs
@@ -1,11 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class GoToDataLoadSceneButton : MonoBehaviour {
 
+	string username;
+	string password;
+	UnityEngine.GameObject errorMessage;
+
 	public void OnClick() {
-		SceneManager.LoadScene("DataLoad");
+		username = UnityEngine.GameObject.Find("Input Username").GetComponent<InputField>().text;
+		password = UnityEngine.GameObject.Find("Input Password").GetComponent<InputField>().text;
+		errorMessage = UnityEngine.GameObject.Find("TextErrorMessage");
+		StartCoroutine(signup("http://0.0.0.0:5000/signup", username, password, "key", "hash"));
 	}
+
+    IEnumerator signup(string url, string username, string password, string public_key, string terminal_hash) {
+		WWWForm form = new WWWForm();
+		form.AddField("username", username);
+		form.AddField("password", password);
+		form.AddField("public_key_base64", public_key);
+		form.AddField("terminal_hash", terminal_hash);
+
+        // accsess
+        WWW www = new WWW(url, form);
+        yield return www;
+		// error
+		if (!string.IsNullOrEmpty(www.error))
+		{
+			errorMessage.GetComponent<Text>().text = "Error. Please Rytry.";
+		}
+		if(www.text == "ok")
+		{
+			SceneManager.LoadScene("DataLoad");			
+		}
+		else
+		{
+			errorMessage.GetComponent<Text>().text = www.text;
+		}
+    }
 }
